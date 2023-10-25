aa06aa53f8f0c1bcd03ec22d5904193299d4d646
guilhermebbaptista <guilherme_b_baptista@estudante.sesisenai.org.br>
2023-10-25 09:45:35 -0300

    ultima atualização login e cadastro

--- Data_base/conexao.php
+++ Data_base/conexao.php
@@ -1,9 +1,9 @@
 <?php
 // Definição das constantes de conexão com o banco de dados
-define('HOST', 'sql105.infinityfree.com');
-define('USUARIO', 'if0_34441522');
-define('SENHA', 't45KwRmQJ1cX3');
-define('DB', 'if0_34441522_cadastro');
+define('HOST', 'sql311.infinityfree.com');
+define('USUARIO', 'if0_35300810');
+define('SENHA', 'fIaWEG2mooKoLT');
+define('DB', 'if0_35300810_ultimo_banco');
  
 // Conexão com o banco de dados utilizando as constantes definidas
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
